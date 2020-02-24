e1df4e32a9dad26080ae8e960e21f0c3d433071f
oguzhantomak <oguzhantomak@gmail.com>
2020-02-24 17:59:11 +0300

    Static files,package.json added and edited. Also bootstrap added to project.

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace MovieApp
@@ -17,12 +19,20 @@ namespace MovieApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles(); // wwwroot klasöründeki klasör ve dosyaları dışarı açar.
+            app.UseStaticFiles(new StaticFileOptions{
+                FileProvider=new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),"node_modules")),
+                RequestPath="/modules"
+            }); // node_modules dosyasını dışarıya açıyoruz. Ve çağırırken modules olarak çağırıyoruz. ÖRN: modules/bootstrap/dist/css/bootstrap.min.css olarak erşiebiliriz artık.
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -30,7 +40,16 @@ namespace MovieApp
 
 
             // home/index/id
-            app.UseMvcWithDefaultRoute();
+            // app.UseMvcWithDefaultRoute();
+
+             app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             // app.UseRouting();
 
